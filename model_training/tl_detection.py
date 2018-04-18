9f6d73545afbef4870adcab2c0da3b8dcf611cd6
Steven Eisinger <etienneshorty@gmail.com>
2018-04-18 00:52:54 -0400

    Added imports to tl_detection.py

--- model_training/tl_detection.py
+++ model_training/tl_detection.py
@@ -1,3 +1,11 @@
+import os
+import sys
 import tensorflow as tf
-import object_detection.utils as dataset_util
+
+# change dir and path as specified in installation instructions
+os.chdir(os.path.join(os.path.dirname(tf.__file__), "models", "research"))
+sys.path.append(os.getcwd())
+sys.path.append(os.path.join(os.getcwd(), "slim"))
+
+from object_detection.utils import dataset_util
 
