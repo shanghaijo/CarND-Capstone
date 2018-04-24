90523470489f0f4b3060e763c1a9b75fcb507bde
Steven Eisinger <etienneshorty@gmail.com>
2018-04-23 22:34:56 -0400

    Playing around with tl_classifier

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -14,6 +14,7 @@ class TLClassifier(object):
 
     def __init__(self):
         self.detection_graph = tf.Graph()
+
         with self.detection_graph.as_default():
             od_graph_def = tf.GraphDef()
             # Works up to here.
@@ -21,12 +22,13 @@ class TLClassifier(object):
                 serialized_graph = fid.read()
                 od_graph_def.ParseFromString(serialized_graph)
                 tf.import_graph_def(od_graph_def, name='')
-            self.image_tensor = self.detection_graph.get_tensor_by_name('image_tensor:0')
-            self.d_boxes = self.detection_graph.get_tensor_by_name('detection_boxes:0')
-            self.d_scores = self.detection_graph.get_tensor_by_name('detection_scores:0')
-            self.d_classes = self.detection_graph.get_tensor_by_name('detection_classes:0')
-            self.num_d = self.detection_graph.get_tensor_by_name('num_detections:0')
         self.sess = tf.Session(graph=self.detection_graph)
+
+        self.image_tensor = self.detection_graph.get_tensor_by_name('image_tensor:0')
+        self.d_boxes = self.detection_graph.get_tensor_by_name('detection_boxes:0')
+        self.d_scores = self.detection_graph.get_tensor_by_name('detection_scores:0')
+        self.d_classes = self.detection_graph.get_tensor_by_name('detection_classes:0')
+        self.num_d = self.detection_graph.get_tensor_by_name('num_detections:0')
 
     def get_classification(self, image):
         """Determines the color of the traffic light in the image
@@ -41,14 +43,19 @@ class TLClassifier(object):
         boxes = scores = classes = None
         num_detections = 0
 
+        img_expanded = np.expand_dims(image, axis=0)
+
         # Bounding Box Detection.
         with self.detection_graph.as_default():
             # Expand dimension since the model expects image to have shape [1, None, None, 3].
-            img_expanded = np.expand_dims(image, axis=0)
             (boxes, scores, classes, num) = self.sess.run(
                 [self.d_boxes, self.d_scores, self.d_classes, self.num_d],
                 feed_dict={self.image_tensor: img_expanded})
-            num_detection = tf.cast(num[0], tf.int32)
+            num_detections = tf.cast(num[0], tf.int32)
+
+        boxes = np.squeeze(boxes)
+        scores = np.squeeze(scores)
+        classes = np.squeeze(classes).astype(np.int32)
 
         if num_detections > 0:
             print(boxes)
